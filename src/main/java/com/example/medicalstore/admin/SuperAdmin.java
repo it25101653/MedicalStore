84863bac609264a63afec96867e5678086081a6c
IT25101190 <it25101190@my.Sliit.lk>
2026-05-15 02:41:36 +0530

    Extend super admin functionality
    Added additional permissions for super admin role

--- src/main/java/com/example/medicalstore/admin/SuperAdmin.java
+++ src/main/java/com/example/medicalstore/admin/SuperAdmin.java
@@ -2,16 +2,24 @@ package com.example.medicalstore.admin;
 
 public class SuperAdmin extends AdminUser {
 
+    // Constructor
     public SuperAdmin(String adminId, String name, String email, String password) {
         super(adminId, name, email, password, "SUPER");
     }
 
+    // Override permission method
     @Override
     public boolean canDeleteUsers() {
         return true;
     }
 
+    // Super admin can manage admins
     public boolean canManageAdmins() {
+        return true;
+    }
+
+    // Super admin can manage reports
+    public boolean canManageReports() {
         return true;
     }
 }
