7fe090a1b43194ad8f5446178f24c6a96a3c9b04
IT25104079 <it25104079@my.sliit.lk>
2026-05-15 12:48:36 +0530

    Update Medicine.java

--- src/main/java/com/example/medicalstore/medicine/Medicine.java
+++ src/main/java/com/example/medicalstore/medicine/Medicine.java
@@ -7,13 +7,15 @@ public abstract class Medicine {
     private String category;
     private double price;
     private int stock;
+    private String description;
 
-    public Medicine(String medId, String name, String category, double price, int stock) {
+    public Medicine(String medId, String name, String category, double price, int stock,String description) {
         this.medId    = medId;
         this.name     = name;
         this.category = category;
         this.price    = price;
         this.stock    = stock;
+        this.description  = description;
     }
 
     public String getMedId(){
@@ -31,6 +33,8 @@ public abstract class Medicine {
     public int getStock(){
         return stock;
     }
+    public String getDescription()    {
+        return description; }
 
     public void setPrice(double price) {
         this.price = price;
@@ -41,15 +45,19 @@ public abstract class Medicine {
     public void setName(String name){
         this.name = name;
     }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public abstract String getDisplayInfo();
 
     public String toFileString() {
-        return medId+","+name+","+category+","+price+","+stock+","+getDisplayInfo();
+        return medId+","+name+","+category+","+price+","+stock+","+getDisplayInfo() + "," + description;
     }
 
     public static Medicine fromFileString(String line) {
-        String[] p = line.split(",");
+        String[] p = line.split(",",7);
+        String desc = p.length > 6 ? p[6] : "No description available.";
         return new OTCMedicine(p[0], p[1], p[2],
                 Double.parseDouble(p[3]),
                 Integer.parseInt(p[4]));
