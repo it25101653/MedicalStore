b59715e1b6b621d0e592798506482404776bb080
it25102628 <it25102628@my.sliit.lk>
2026-05-15 23:19:40 +0530

    Remove update method from ReviewService.  reviews immutable after submission

--- src/main/java/com/example/medicalstore/review/ReviewService.java
+++ src/main/java/com/example/medicalstore/review/ReviewService.java
@@ -1,10 +1,12 @@
 package com.example.medicalstore.review;
 
 import org.springframework.stereotype.Service;
+
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+//review service class
 @Service
 public class ReviewService {
 
@@ -29,23 +31,16 @@ public class ReviewService {
                 .collect(Collectors.toList());
     }
 
-    public void addReview(String userId, String medId, int rating, String comment) {
+    //add review method
+    public void addReview(String userName,String userEmails, String medId, int rating, String comment) {
+        new File("data").mkdirs();
         String id = "REV" + System.currentTimeMillis();
-        VerifiedReview r = new VerifiedReview(id, userId, medId, rating, comment);
+        VerifiedReview r = new VerifiedReview(id, userName, userEmails, medId, rating, comment);
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE, true))) {
             bw.write(r.toFileString()); bw.newLine();
         } catch (IOException e) { e.printStackTrace(); }
     }
 
-    public void update(String reviewId, int newRating, String newComment) {
-        List<Review> list = getAllReviews();
-        for (Review r : list)
-            if (r.getReviewId().equals(reviewId)) {
-                r.setRating(newRating);
-                r.setComment(newComment);
-            }
-        saveAll(list);
-    }
 
     public void delete(String reviewId) {
         List<Review> list = getAllReviews().stream()
