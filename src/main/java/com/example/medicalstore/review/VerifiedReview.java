a234c579aeb5bba29f7b82609f1c783db309d33c
it25102628 <it25102628@my.sliit.lk>
2026-05-17 21:29:44 +0530

    updated constructor for likes/dislikes

--- src/main/java/com/example/medicalstore/review/VerifiedReview.java
+++ src/main/java/com/example/medicalstore/review/VerifiedReview.java
@@ -3,8 +3,8 @@ package com.example.medicalstore.review;
 public class VerifiedReview extends Review {
 
     public VerifiedReview(String reviewId, String userName, String userEmails,
-                          String medId, int rating, String comment, String date) {
-        super(reviewId, userName,userEmails, medId, rating, comment,date);
+                          String medId, int rating, String comment, String date, int likes, int dislikes) {
+        super(reviewId, userName,userEmails, medId, rating, comment,date,likes, dislikes);
     }
 
     @Override
