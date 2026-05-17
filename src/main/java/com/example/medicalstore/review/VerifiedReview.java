c80e8c75ee2b01892d69a93bca9414e4afcfa64c
it25102628 <it25102628@my.sliit.lk>
2026-05-17 11:08:11 +0530

    Update VerifiedReview constructor to date field

--- src/main/java/com/example/medicalstore/review/VerifiedReview.java
+++ src/main/java/com/example/medicalstore/review/VerifiedReview.java
@@ -3,8 +3,8 @@ package com.example.medicalstore.review;
 public class VerifiedReview extends Review {
 
     public VerifiedReview(String reviewId, String userName, String userEmails,
-                          String medId, int rating, String comment) {
-        super(reviewId, userName,userEmails, medId, rating, comment);
+                          String medId, int rating, String comment, String date) {
+        super(reviewId, userName,userEmails, medId, rating, comment,date);
     }
 
     @Override
